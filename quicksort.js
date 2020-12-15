d1d7135c047842a4b80504d940ca465c44e85172
Sathish Gajendran <Sathish.gajendran@gmail.com>
2020-12-15 13:16:29 +0530

    Update quicksort.js

--- quicksort.js
+++ quicksort.js
@@ -1,13 +1,13 @@
-// let array = [20, 22, 10, 6, 2, 9, 4, 31, 25, 3];
-let array = [3,44,38,5,47,15,36,26,27,2,46,4,19,50,48];
+// const array = [20, 22, 10, 6, 2, 9, 4, 31, 25, 3];
+const array = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48];
 
-let pivot = (arr)=>{
+const pivot = (arr)=>{
+    const firstElement = arr[0];
     let pi = 0;
-    let firstElement = arr[0];
 
     for (let i = 1; i < arr.length; i++) {
         if (arr[i] < firstElement) {
-            let temp = arr[pi + 1];
+            const temp = arr[pi + 1];
             arr[pi + 1] = arr[i];
             arr[i] = temp;
             pi++;
@@ -23,14 +23,14 @@ let pivot = (arr)=>{
     };
 }
 
-let quicksort = (array)=>{
-    if (array.length < 2)
+const quicksort = (array)=>{
+    if (array.length < 2) {
         return array;
-    let {pi, arr} = pivot(array);
+    }
+    const {pi, arr} = pivot(array);
     return [...quicksort(arr.slice(0, pi)), arr[pi], ...quicksort(arr.slice(pi + 1))];
-}
-;
+};
 
-let result = quicksort(array);
+const result = quicksort(array);
 console.log(array);
 console.log(result);
